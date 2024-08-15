e51102d059bf05b8a3063f9836f8e369ba09d646
rafaewaqarqazi <rafaewaqar@gmail.com>
2024-08-15 15:19:55 +0500

    move: auth options in separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Main from "@/components/Main";
 import { redirect } from "next/navigation";
 import { Roles } from "@/utils/enums";
+import { authOptions } from "@/app/api/auth/[...nextauth]/actions";
 export default async function Home() {
   const session = await getServerSession(authOptions);
   console.log({ session });
--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import React, { FC, PropsWithChildren } from "react";
 import { getServerSession } from "next-auth";
 import { Roles } from "@/utils/enums";
 import { redirect } from "next/navigation";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/actions";
 const AuthLayout: FC<PropsWithChildren> = async ({ children }) => {
   const session = await getServerSession(authOptions);
   console.log({ session });
